51e40684a886ed6e9a05d9e52f83c32ac320eaee
Eddie MAUNE <emaune@c4r14s10.wethinkcode.co.za>
2019-12-12 12:05:20 +0200

    converted mongoose to mongo client

--- backend/server.js
+++ backend/server.js
@@ -5,65 +5,98 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const todoRoutes = express.Router();
 const Todo = require('./todo.model.js');
-
+const mongodb = require('mongodb');
+const MongoClient = require('mongodb').MongoClient
 const PORT = 4000;
+var db = null;
 
-mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true , useUnifiedTopology: true});
-const connection = mongoose.connection;
 
-connection.once('open', function() {
-    console.log("MongoDB database connection established successfully");
+// mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true , useUnifiedTopology: true});
+// const connection = mongoose.connection;
+
+// connection.once('open', function() {
+//     console.log("MongoDB database connection established successfully");
+// });
+
+MongoClient.connect("mongodb://localhost:27017/todos", function(err, client) {
+  if(err) return console.error(err);
+
+  db = client.db('todos');
+//   console.log(db);
+
+  // the Mongo driver recommends starting the server here because most apps *should* fail to start if they have no DB.  If yours is the exception, move the server startup elsewhere. 
 });
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/todos', todoRoutes);
 
-todoRoutes.route('/').get(function(req, res) {
-    Todo.find(function(err, todos) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.json(todos);
+
+todoRoutes.route('/').get(function(req, res, next) {
+    ///console.log(db.collection('todos').find({}));
+   
+    db.collection('todos').find({}, function(err, docs) {
+    //   if(err) 
+    //     return next(err);
+       docs.each(function(err, doc) {
+        if(doc) {
+          console.log(doc);
         }
-    });
-});
-
-todoRoutes.route('/:id').get(function(req, res) {
-    let id = req.params.id;
-    Todo.findById(id, function(err, todo) {
-        res.json(todo);
-    });
-});
-
-todoRoutes.route('/add').post(function(req, res) {
-    let todo = new Todo(req.body);
-    todo.save()
-        .then(todo => {
-            res.status(200).json({'todo': 'todo added successfully'});
-        })
-        .catch(err => {
-            res.status(400).send('adding new todo failed');
+        else {
+          res.end();
+        }
         });
-});
-
-todoRoutes.route('/update/:id').post(function(req, res) {
-    Todo.findById(req.params.id, function(err, todo) {
-        if (!todo)
-            res.status(404).send("data is not found");
-        else
-            todo.todo_description = req.body.todo_description;
-            todo.todo_responsible = req.body.todo_responsible;
-            todo.todo_priority = req.body.todo_priority;
-            todo.todo_completed = req.body.todo_completed;
-            todo.save().then(todo => {
-                res.json('Todo updated!');
-            })
-            .catch(err => {
-                res.status(400).send("Update not possible");
-            });
     });
-});
+    res.json("OK");
+   });
+
+
+
+// todoRoutes.route('/').get(function(req, res) {
+//     Todo.find(function(err, todos) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             res.json(todos);
+//         }
+//     });
+// });
+
+// todoRoutes.route('/:id').get(function(req, res) {
+//     let id = req.params.id;
+//     Todo.findById(id, function(err, todo) {
+//         res.json(todo);
+//     });
+// });
+
+// todoRoutes.route('/add').post(function(req, res) {
+//     let todo = new Todo(req.body);
+//     todo.save()
+//         .then(todo => {
+//             res.status(200).json({'todo': 'todo added successfully'});
+//         })
+//         .catch(err => {
+//             res.status(400).send('adding new todo failed');
+//         });
+// });
+
+// todoRoutes.route('/update/:id').post(function(req, res) {
+//     Todo.findById(req.params.id, function(err, todo) {
+//         if (!todo)
+//             res.status(404).send("data is not found");
+//         else
+//             todo.todo_description = req.body.todo_description;
+//             todo.todo_responsible = req.body.todo_responsible;
+//             todo.todo_priority = req.body.todo_priority;
+//             todo.todo_completed = req.body.todo_completed;
+//             todo.save().then(todo => {
+//                 res.json('Todo updated!');
+//             })
+//             .catch(err => {
+//                 res.status(400).send("Update not possible");
+//             });
+//     });
+// });
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
