8a702c0b9ea12a3aa5696c1b21bce7d705ec7238
Eddie Maune <eddiemaune@gmail.com>
2019-12-20 15:58:49 +0200

    setting up redux for state management

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,45 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-
-import TodosList from "./components/TodoList";
-import EditTodo from "./components/edit";
-import CreateTodo from "./components/create";
-import logo from './logo.svg';
-import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-
-//hmmm
-function App() {
-  return (
-   
-    <Router>
-       <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank" rel="noopener noreferrer">
-              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
-            </a>
-            <Link to="/" className="navbar-brand">Matcha</Link>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">Todos</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Todo</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <br/>
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/shared/Navbar";
+import Landing from "./components/shared/Landing";
+import Register from "./containers/auth/Register";
+import Login from "./containers/auth/Login";
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
         </div>
-      <Route path="/" exact component={TodosList} />
-      <Route path="/edit/:id" component={EditTodo} />
-      <Route path="/create" component={CreateTodo} />
-     
-    </Router>
-
-  );
+      </Router>
+    );
+  }
 }
-
 export default App;
